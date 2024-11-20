63547864a2e664c25d8e3eb8b7c86b99a050539c
0xshiku <olachico@icloud.com>
2024-11-20 23:41:44 +0000

    test: introduce tests for service in payments

---
+++ payments/service_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"common/api"
+	"context"
+	"payments/processor/inmem"
+	"testing"
+)
+
+func TestService(t *testing.T) {
+	processor := inmem.NewInmem()
+	svc := NewService(processor)
+
+	t.Run("should create a payment link", func(t *testing.T) {
+		link, err := svc.CreatePayment(context.Background(), &api.Order{})
+		if err != nil {
+			t.Errorf("CreatePayment() error = %v, want nil", err)
+		}
+
+		if link == "" {
+			t.Errorf("CreatePayment() link is empty")
+		}
+	})
+}
